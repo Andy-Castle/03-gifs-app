a074e7b7fcb38a4f2e786a6913ffa4526b5bf628
Andy Castle <masterchief_and@live.com>
2025-01-21 16:27:05 -0500

    se creo la estructura del proyecto de gifs app

---
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'shared-sidebar',
+  templateUrl: './sidebar.component.html',
+  standalone: false,
+  styleUrl: './sidebar.component.css',
+})
+export class SidebarComponent {}
