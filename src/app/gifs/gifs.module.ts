4fbfc1b1d2efd7558473f01a65af3118924e4f72
Andy Castle <masterchief_and@live.com>
2025-01-21 16:40:46 -0500

    se sigue dando estructura al proyecto de gifs app, ya tiene una mejor vista

--- src/app/gifs/gifs.module.ts
+++ src/app/gifs/gifs.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { HomePageComponent } from './pages/home/home.component';
+import { SearchBoxComponent } from './components/search-box/searc-box.component';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [HomePageComponent, SearchBoxComponent],
+  imports: [CommonModule],
+  exports: [HomePageComponent],
 })
-export class GifsModule { }
+export class GifsModule {}
