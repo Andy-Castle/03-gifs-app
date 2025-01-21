835e78e035fbacb0b4a5d63281b7189c860ef6b5
Andy Castle <masterchief_and@live.com>
2025-01-21 17:02:22 -0500

    @ViewChild sirve para tomar una referencia local, el ViewChildren toma todos los elementos input los los regresa en un arreglo

--- src/app/gifs/components/search-box/searc-box.component.ts
+++ src/app/gifs/components/search-box/searc-box.component.ts
@@ -1,13 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+
+// @Component({
+//   selector: 'gifs-search-box',
+//   standalone: false,
+//   template: `
+//     <h5>Buscar:</h5>
+//     <input
+//       type="text"
+//       class="form-control"
+//       placeholder="Buscar gifs..."
+//       (keyup.enter)="searchTag(txtTagInput.value)"
+//       #txtTagInput
+//     />
+//   `,
+// })
 
 @Component({
   selector: 'gifs-search-box',
   standalone: false,
   template: `
     <h5>Buscar:</h5>
-    <input type="text" class="form-control" placeholder="Buscar gifs..." />
+    <input
+      type="text"
+      class="form-control"
+      placeholder="Buscar gifs..."
+      (keyup.enter)="searchTag()"
+      #txtTagInput
+    />
   `,
 })
 export class SearchBoxComponent {
+  @ViewChild('txtTagInput')
+  public tagInput!: ElementRef<HTMLInputElement>;
+
   constructor() {}
+
+  // searchTag(newTag: string) {
+  //   console.log({ newTag });
+  // }
+
+  searchTag() {
+    const newTag = this.tagInput.nativeElement.value;
+    console.log({ newTag });
+  }
 }
